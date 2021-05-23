f9e8c37d4488d33ea8c434c31608d4e85c9a529c
RafaelaFernandaSilva <rafaelafernandasilveira@gmail.com>
2021-05-23 14:55:36 -0300

    Projeto final bloco 1

---
+++ Projeto_bloco-1/src/pacote/DevJunior.java
@@ -0,0 +1,11 @@
+package pacote;
+
+public class DevJunior extends Funcionarios{
+	
+	public DevJunior() {
+		
+		super("Desenvolvedor Java Jr",3000.00);
+	}
+	
+	
+}
---
+++ Projeto_bloco-1/src/pacote/DevSenior.java
@@ -0,0 +1,10 @@
+package pacote;
+
+public class DevSenior extends Funcionarios{
+	
+	public DevSenior() {
+		super("Desenvolvedor Senior",12000.00);
+	}
+	
+}
+
